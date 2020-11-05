c4d1051133471dae53eeec7565ab4e0ac0d1bf1b
pintoedo <eduardoandrespo@gmail.com>
2020-11-05 14:09:53 +0100

    add build

---
+++ build/precache-manifest.4df3636feb649f89ab8c970ebce00437.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0e4b8adc8876a3d5c585e37291fc9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16f61bd2eb8096b55c9a",
+    "url": "/static/css/main.6f4a16ec.chunk.css"
+  },
+  {
+    "revision": "021796e2d6f0a6a1ce41",
+    "url": "/static/js/2.aa96f0d0.chunk.js"
+  },
+  {
+    "revision": "16f61bd2eb8096b55c9a",
+    "url": "/static/js/main.3bafef53.chunk.js"
+  },
+  {
+    "revision": "5306d471d6f4bf8dbc37",
+    "url": "/static/js/runtime~main.1e9280f6.js"
+  },
+  {
+    "revision": "408c8622707dc07c9c050a5f2908a217",
+    "url": "/static/media/fleetspu.408c8622.png"
+  },
+  {
+    "revision": "fffe3e692ddfa9c92b09065f87f92e86",
+    "url": "/static/media/karamobile.fffe3e69.gif"
+  },
+  {
+    "revision": "b922f9b331758298fde38903292351eb",
+    "url": "/static/media/spaceway-mobile.b922f9b3.gif"
+  }
+]);
